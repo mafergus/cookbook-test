222267d5448bc846d2ec7272b1f4f737393e97eb
ngodi <albertngodi@gmail.com>
2020-05-29 15:53:30 +0100

    added imageurl and ingredients to recipe dataset

--- src/RecipeDetails/RecipeDetails.js
+++ src/RecipeDetails/RecipeDetails.js
@@ -2,12 +2,30 @@ import React from "react";
 
 import './RecipeDetails.css';
 
-const RecipeDetails = ({name, author, description }) => {
+const RecipeDetails = ({name, author, description, imageUrl, ingredients }) => {
     return(
-        <div className="recipe-details-info">
-           <div className="name"> {name} </div>
-           <span>By {author}</span>
-           <p>{description}</p>
+        <div className="recipe-data">
+            <div className="recipe-image">
+             <div className="name"> {name} </div>
+              <img src={imageUrl} width="300px" />
+              <span>By {author}</span>
+            </div>
+            <div className="recipe-info">
+              <p>{description}</p>
+              <div className="ingredients">
+                  <h3>Ingredients</h3>
+                  <ul>
+                      {
+                          ingredients.map(ingredient => {
+                              return(
+                                  <li>ingredient</li>
+                              )
+                          })
+                      }
+                  </ul>
+              </div>
+            </div>
+           
         </div>
     )
 }
